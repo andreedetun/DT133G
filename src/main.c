67bb26c4025196e4f8659ba97fa8ac043d471414
GkevinG <kevinpguvenal@gmail.com>
2021-02-08 09:32:32 +0100

    a test and a "question" in main.c.

--- src/main.c
+++ src/main.c
@@ -3,6 +3,9 @@
 #include "list/list.h"
 
 int main(){
+    /* Is this how you inst. the lists, to test for maximum,minimum and so on?
+     * int L1[6] = {3, 1, 5, 10, 8, 7};
+    int L2[6] = {5, 2, 9, 6, 1, 2}; (this is basically just to test githubs push for me)*/
     List* list = initList();
     insert(list, 20);
     insert(list, 40);
